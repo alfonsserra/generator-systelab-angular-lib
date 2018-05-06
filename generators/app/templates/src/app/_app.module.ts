da626276615a7495dbe2824bbdeae8a1a1856b9b
alfonsserra <alfonsoserra@gmail.com>
2018-05-06 11:32:29 +0200

    Import the correct module

--- generators/app/templates/src/app/_app.module.ts
+++ generators/app/templates/src/app/_app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { SystelabPreferencesModule } from './<%= title %>/<%= title %>.module';
+import { <%= module %> } from './<%= title %>/<%= title %>.module';
 import { SampleService } from './<%= title %>/sample.service';
 
 
